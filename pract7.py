867f2c9364fd2ffdba66ef9130df0d056d7d6dc7
manighahrmani <mashapicasso@gmail.com>
2023-06-24 17:23:09 +0100

    Update pract7.py

--- pract7.py
+++ pract7.py
@@ -32,6 +32,35 @@ def infiniteLoop():
         print(i)
 
 
+def addUpNumbers1():
+    total = 0
+    moreNumbers = "y"
+    while moreNumbers == "y":
+        number = int(input("Enter a number "))
+        total = total + number
+        moreNumbers = input("Any more numbers? ")
+    print("The total is", total)
+
+
+def addUpNumbers2():
+    total = 0
+    number = int(input("Number (0 to stop): "))
+    while number != 0:
+        total = total + number
+        number = eval(input("Number (0 to stop): "))
+    print("The total is", total)
+
+
+def addUpNumbers3():
+    total = 0
+    nStr = input("Number (return to stop): ")
+    while nStr != "":
+        number = eval(nStr)
+        total = total + number
+        nStr = input("Number (return to stop): ")
+    print("The total is", total)
+
+
 # For exercise 2
 def trafficLights():
     win = GraphWin()
